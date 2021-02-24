c80e1c8af748b582e5152b1e06cf4ac11e40f713
Petr Plechac <plechac@ucl.cas.cz>
2021-02-24 23:38:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
   author = 'Petr Plechac',                   
   author_email = 'plechac@ucl.cas.cz',      
   url = 'https://github.com/versotym/rhymetagger',
-  download_url = 'https://github.com/versotym/rhymeTagger/archive/v_01.tar.gz',
-  keywords = ['poetry', 'rhyme', 'versification'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  download_url = 'https://github.com/versotym/rhymeTagger/archive/v0.2.tar.gz',
+  keywords = ['poetry', 'rhyme', 'versification'],   
+  install_requires=[            
           'ujson',
           'string',
           'nltk',
@@ -20,11 +20,10 @@ setup(
           'ast',
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
     'Topic :: Text Processing :: Linguistic',
     'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',      
   ],
 )
-view rawsetup.py hosted with ❤ by GitHub
